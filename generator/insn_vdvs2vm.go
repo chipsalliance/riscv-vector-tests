ed734125f3bd9644df6f834aa26e0887919eb3a0
Yang Liu <numbksco@gmail.com>
2022-11-18 11:46:07 +0800

    Add vfwcvt* insns

--- generator/insn_vdvs2vm.go
+++ generator/insn_vdvs2vm.go
@@ -2,11 +2,18 @@ package generator
 
 import (
 	"fmt"
+	"math"
 	"strings"
 )
 
 func (i *insn) genCodeVdVs2Vm() []string {
-	combinations := i.combinations(allLMULs, floatSEWs, []bool{false, true})
+	vdWidening := strings.HasPrefix(i.Name, "vfw")
+	vs2Widening := strings.HasSuffix(i.Name, "wf")
+	vdSize := iff(vdWidening, 2, 1)
+
+	lmuls := iff(vdWidening || vs2Widening, wideningMULs, allLMULs)
+	sews := iff(vdWidening || vs2Widening, floatSEWs[:len(floatSEWs)-1], floatSEWs)
+	combinations := i.combinations(lmuls, sews, []bool{false, true})
 
 	res := make([]string, 0, len(combinations))
 	for _, c := range combinations {
@@ -17,10 +24,13 @@ func (i *insn) genCodeVdVs2Vm() []string {
 		builder.WriteString(i.gWriteRandomData(LMUL(1)))
 		builder.WriteString(i.gLoadDataIntoRegisterGroup(0, c.LMUL1, SEW(8)))
 
-		vd := int(c.LMUL1)
-		vs2 := int(c.LMUL1) * 2
-		builder.WriteString(i.gWriteRandomData(LMUL(1)))
-		builder.WriteString(i.gLoadDataIntoRegisterGroup(vd, c.LMUL1, SEW(8)))
+		vdEMUL1 := LMUL(math.Max(float64(int(c.LMUL)*vdSize), 1))
+		vdEEW := c.SEW * SEW(vdSize)
+		vd := int(vdEMUL1)
+		vs2 := vd * 2
+
+		builder.WriteString(i.gWriteRandomData(vdEMUL1))
+		builder.WriteString(i.gLoadDataIntoRegisterGroup(vd, vdEMUL1, SEW(8)))
 
 		builder.WriteString(i.gWriteTestData(false, c.LMUL1, c.SEW, 0))
 		builder.WriteString(i.gLoadDataIntoRegisterGroup(vs2, c.LMUL1, c.SEW))
@@ -31,7 +41,7 @@ func (i *insn) genCodeVdVs2Vm() []string {
 			i.Name, vd, vs2, v0t(c.Mask)))
 		builder.WriteString("# -------------- TEST END   --------------\n")
 
-		builder.WriteString(i.gStoreRegisterGroupIntoData(vd, c.LMUL1, c.SEW))
+		builder.WriteString(i.gStoreRegisterGroupIntoData(vd, vdEMUL1, vdEEW))
 		builder.WriteString(i.gMagicInsn(vd))
 
 		res = append(res, builder.String())
