cc9f115147ee98c951938124b516de75d336d5a1
Yang Liu <numbksco@gmail.com>
2022-11-18 14:58:22 +0800

    Add more insns

--- generator/insn.go
+++ generator/insn.go
@@ -55,11 +55,12 @@ const (
 	insnFormatVdFs1        insnFormat = "vd,fs1" // Added
 	insnFormatVdImm        insnFormat = "vd,imm"
 	insnFormatVdVs2        insnFormat = "vd,vs2"
-	insnFormatVdVs2Vm      insnFormat = "vd,vs2,vm" // Added
-	insnFormatRdVs2Vm      insnFormat = "rd,vs2,vm" // Added
-	insnFormatRdVs2        insnFormat = "rd,vs2"    // Added
-	insnFormatFdVs2        insnFormat = "fd,vs2"    // Added
-	insnFormatVdVm         insnFormat = "vd,vm"     // Added
+	insnFormatVdVs2Vm      insnFormat = "vd,vs2,vm"   // Added
+	insnFormatVdVs2VmP2    insnFormat = "vd,vs2,vm/2" // Added
+	insnFormatRdVs2Vm      insnFormat = "rd,vs2,vm"   // Added
+	insnFormatRdVs2        insnFormat = "rd,vs2"      // Added
+	insnFormatFdVs2        insnFormat = "fd,vs2"      // Added
+	insnFormatVdVm         insnFormat = "vd,vm"       // Added
 )
 
 var formats = map[insnFormat]struct{}{
@@ -135,6 +136,8 @@ func (i *Insn) genTestCases() []string {
 		return i.genCodeVdFs1()
 	case insnFormatVdVs2Vm:
 		return i.genCodeVdVs2Vm()
+	case insnFormatVdVs2VmP2:
+		return i.genCodeVdVs2VmP2()
 	case insnFormatRdVs2Vm:
 		return i.genCodeRdVs2Vm()
 	case insnFormatRdVs2:
---
+++ generator/insn_vdvs2vmp2.go
@@ -0,0 +1,44 @@
+package generator
+
+import (
+	"fmt"
+	"strings"
+)
+
+func (i *Insn) genCodeVdVs2VmP2() []string {
+	vdMask := strings.HasPrefix(i.Name, "vm")
+
+	combinations := i.combinations([]LMUL{1}, []SEW{8}, []bool{false, true})
+	if !vdMask {
+		combinations = i.combinations(allLMULs, allSEWs, []bool{false, true})
+	}
+
+	res := make([]string, 0, len(combinations))
+	for _, c := range combinations {
+		builder := strings.Builder{}
+		builder.WriteString(i.gTestDataAddr())
+		builder.WriteString(c.comment())
+
+		vd := int(c.LMUL1)
+		vs2 := 2 * int(c.LMUL1)
+		builder.WriteString(i.gWriteRandomData(LMUL(1) * 2))
+		builder.WriteString(i.gLoadDataIntoRegisterGroup(0, c.LMUL1, c.SEW))
+		builder.WriteString(fmt.Sprintf("addi a0, a0, %d\n", 1*i.vlenb()))
+		builder.WriteString(i.gLoadDataIntoRegisterGroup(vs2, c.LMUL1, c.SEW))
+
+		builder.WriteString(i.gWriteRandomData(c.LMUL1))
+		builder.WriteString(i.gLoadDataIntoRegisterGroup(vd, c.LMUL1, c.SEW))
+
+		builder.WriteString("# -------------- TEST BEGIN --------------\n")
+		builder.WriteString(i.gVsetvli(c.Vl, c.SEW, c.LMUL))
+		builder.WriteString(fmt.Sprintf("%s v%d, v%d%s\n",
+			i.Name, vd, vs2, v0t(c.Mask)))
+		builder.WriteString("# -------------- TEST END   --------------\n")
+
+		builder.WriteString(i.gStoreRegisterGroupIntoData(vd, c.LMUL1, c.SEW))
+		builder.WriteString(i.gMagicInsn(vd))
+
+		res = append(res, builder.String())
+	}
+	return res
+}
