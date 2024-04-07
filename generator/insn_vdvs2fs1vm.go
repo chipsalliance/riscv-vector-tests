f9620b503aec847ab76bb704ff09aa1f541f1c2a
Yang Liu <liuyang22@iscas.ac.cn>
2024-04-07 16:23:03 +0800

    Fix VdVs2Fs1Vm vs2 widening handling (for #32)

--- generator/insn_vdvs2fs1vm.go
+++ generator/insn_vdvs2fs1vm.go
@@ -10,6 +10,7 @@ func (i *Insn) genCodeVdVs2Fs1Vm(pos int) []string {
 	vdWidening := strings.HasPrefix(i.Name, "vfw")
 	vs2Widening := strings.HasSuffix(i.Name, ".wf")
 	vdSize := iff(vdWidening, 2, 1)
+	vs2Size := iff(vs2Widening, 2, 1)
 
 	sews := iff(vdWidening || vs2Widening, floatSEWs[:len(floatSEWs)-1], floatSEWs)
 	combinations := i.combinations(
@@ -28,7 +29,9 @@ func (i *Insn) genCodeVdVs2Fs1Vm(pos int) []string {
 		builder.WriteString(i.gLoadDataIntoRegisterGroup(0, LMUL(1), SEW(32)))
 
 		vdEMUL1 := LMUL(math.Max(float64(int(c.LMUL)*vdSize), 1))
+		vs2EMUL1 := LMUL(math.Max(float64(int(c.LMUL)*vs2Size), 1))
 		vdEEW := c.SEW * SEW(vdSize)
+		vs2EEW := c.SEW * SEW(vs2Size)
 		if vdEEW > SEW(i.Option.XLEN) {
 			res = append(res, "")
 			continue
@@ -41,8 +44,8 @@ func (i *Insn) genCodeVdVs2Fs1Vm(pos int) []string {
 			builder.WriteString(i.gWriteRandomData(vdEMUL1))
 			builder.WriteString(i.gLoadDataIntoRegisterGroup(vd, vdEMUL1, SEW(8)))
 
-			builder.WriteString(i.gWriteTestData(true, !i.NoTestfloat3, r != 0, c.LMUL1, c.SEW, 0, 1))
-			builder.WriteString(i.gLoadDataIntoRegisterGroup(vs2, c.LMUL1, c.SEW))
+			builder.WriteString(i.gWriteTestData(true, !i.NoTestfloat3, r != 0, vs2EMUL1, vs2EEW, 0, 1))
+			builder.WriteString(i.gLoadDataIntoRegisterGroup(vs2, vs2EMUL1, vs2EEW))
 
 			cases := i.testCases(true, c.SEW)
 			for a := 0; a < len(cases); a++ {
