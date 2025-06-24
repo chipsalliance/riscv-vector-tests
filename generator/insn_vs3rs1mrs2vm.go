c904418b7ae055ddfcce140ef376652f0b03005f
Akif Ejaz <akifejaz40@gmail.com>
2025-06-24 12:34:10 +0500

    revert store templates

--- generator/insn_vs3rs1mrs2vm.go
+++ generator/insn_vs3rs1mrs2vm.go
@@ -3,7 +3,6 @@ package generator
 import (
 	"fmt"
 	"strings"
-	"math"
 )
 
 func (i *Insn) genCodeVs3Rs1mRs2Vm(pos int) []string {
@@ -18,50 +17,33 @@ func (i *Insn) genCodeVs3Rs1mRs2Vm(pos int) []string {
 
 	for _, c := range combinations[pos:] {
 		builder := strings.Builder{}
-		var sew SEW = 8 // sew is data width, c.SEW is offset width
-		for ; sew <= SEW(i.Option.XLEN); sew <<= 1 {
-			if int(sew) > int(float64(i.Option.XLEN)*float64(c.LMUL)) {
-				continue
+		builder.WriteString(c.initialize())
+
+		builder.WriteString(i.gWriteRandomData(LMUL(1)))
+		builder.WriteString(i.gLoadDataIntoRegisterGroup(0, LMUL(1), SEW(32)))
+
+		vs3 := int(c.LMUL1)
+		for _, s := range []int{1, maxStride} {
+			builder.WriteString(i.gWriteIntegerTestData(c.LMUL1*LMUL(nfields), c.SEW, 0))
+			for nf := 0; nf < nfields; nf++ {
+				builder.WriteString(i.gLoadDataIntoRegisterGroup(vs3+(nf*int(c.LMUL1)), c.LMUL1, c.SEW))
 			}
-			emul := (float64(c.SEW) / float64(sew)) * float64(c.LMUL) * float64(nfields)
-			if emul > 8 || emul < 1./8 {
-				continue
-			}
-			emul = math.Max(emul, 1) // offset lmul
-			builder.WriteString(c.initialize())
 
-			builder.WriteString(i.gWriteRandomData(LMUL(1)))
-			builder.WriteString(i.gLoadDataIntoRegisterGroup(0, LMUL(1), SEW(32)))
+			builder.WriteString(i.gResultDataAddr())
 
-			lmul1 := LMUL(math.Max((float64(c.SEW)/float64(sew))*float64(c.LMUL), 1))
-			vs3 := int(lmul1)
+			builder.WriteString("# -------------- TEST BEGIN --------------\n")
+			builder.WriteString(fmt.Sprintf("li s0, %d # stride\n", s*int(i.vlenb())*int(c.LMUL1)))
+			builder.WriteString(i.gVsetvli(c.Vl, c.SEW, c.LMUL))
+			builder.WriteString(fmt.Sprintf("%s v%d, (a0), s0%s\n", i.Name, vs3, v0t(c.Mask)))
+			builder.WriteString("# -------------- TEST END   --------------\n")
 
-			for _, s := range []int{1, maxStride} {
-				builder.WriteString(i.gWriteIntegerTestData(lmul1*LMUL(nfields), sew, 0))
-				
-				for nf := 0; nf < nfields; nf++ {
-					builder.WriteString(i.gLoadDataIntoRegisterGroup(vs3+(nf*int(lmul1)), lmul1, sew))
-					builder.WriteString(fmt.Sprintf("li a5, %d\n", i.vlenb()*int(lmul1)))
-					builder.WriteString("add a0, a0, a5\n")
-				}
-
-				builder.WriteString(i.gResultDataAddr())
-
-				builder.WriteString("# -------------- TEST BEGIN --------------\n")
-				builder.WriteString(fmt.Sprintf("li s0, %d # stride\n", s*int(i.vlenb())*int(lmul1)))
-				builder.WriteString(i.gVsetvli(c.Vl, sew, c.LMUL))
-				builder.WriteString(fmt.Sprintf("%s v%d, (a0), s0%s\n", i.Name, vs3, v0t(c.Mask)))
-				builder.WriteString("# -------------- TEST END   --------------\n")
-
-				builder.WriteString("mv a4, a0\n") // Save base address
-				for nf := 0; nf < nfields; nf++ {
-					builder.WriteString(i.gLoadDataIntoRegisterGroup(vs3+(nf*int(lmul1)), lmul1, sew))
-					builder.WriteString(i.gMagicInsn(vs3+(nf*int(lmul1)), lmul1))
-					
-					if nf < nfields-1 {
-						builder.WriteString(fmt.Sprintf("li a5, %d\n", i.vlenb()*int(lmul1)))
-						builder.WriteString("add a0, a4, a5\n")
-					}
+			builder.WriteString("mv a4, a0\n")
+			for nf := 0; nf < nfields; nf++ {
+				for a := 0; a < maxStride; a++ {
+					builder.WriteString(i.gLoadDataIntoRegisterGroup(vs3+(nf*int(c.LMUL1)), c.LMUL1, c.SEW))
+					builder.WriteString(i.gMagicInsn(vs3+(nf*int(c.LMUL1)), c.LMUL1))
+					builder.WriteString(fmt.Sprintf("li a5, %d\n", i.vlenb()*int(c.LMUL1)))
+					builder.WriteString(fmt.Sprintf("add a0, a4, a5\n"))
 				}
 			}
 		}
--- generator/insn_vs3rs1mvm.go
+++ generator/insn_vs3rs1mvm.go
@@ -3,7 +3,6 @@ package generator
 import (
 	"fmt"
 	"strings"
-	"math"
 )
 
 func (i *Insn) genCodeVs3Rs1mVm(pos int) []string {
@@ -18,43 +17,30 @@ func (i *Insn) genCodeVs3Rs1mVm(pos int) []string {
 
 	for _, c := range combinations[pos:] {
 		builder := strings.Builder{}
-		var sew SEW = 8 // sew is data width, c.SEW is offset width
-		for ; sew <= SEW(i.Option.XLEN); sew <<= 1 {
-			if int(sew) > int(float64(i.Option.XLEN)*float64(c.LMUL)) {
-				continue
-			}
-			emul := (float64(c.SEW) / float64(sew)) * float64(c.LMUL) * float64(nfields)
-			if emul > 8 || emul < 1./8 {
-				continue
-			}
-			emul = math.Max(emul, 1) // offset lmul
-			builder.WriteString(c.initialize())
+		builder.WriteString(c.initialize())
 
-			builder.WriteString(i.gWriteRandomData(LMUL(1)))
-			builder.WriteString(i.gLoadDataIntoRegisterGroup(0, LMUL(1), SEW(32)))
+		builder.WriteString(i.gWriteRandomData(LMUL(1)))
+		builder.WriteString(i.gLoadDataIntoRegisterGroup(0, LMUL(1), SEW(32)))
 
-			lmul1 := LMUL(math.Max((float64(c.SEW)/float64(sew))*float64(c.LMUL), 1))
-			vs3 := int(lmul1)
+		vs3 := int(c.LMUL1)
 
-			builder.WriteString(i.gWriteIntegerTestData(lmul1*LMUL(nfields), sew, 0))			
-			for nf := 0; nf < nfields; nf++ {
-				builder.WriteString(i.gLoadDataIntoRegisterGroup(vs3+(nf*int(lmul1)), lmul1, sew))
-				builder.WriteString(fmt.Sprintf("li a5, %d\n", i.vlenb()*int(lmul1)))
-				builder.WriteString("add a0, a0, a5\n")
-			}
-
-			builder.WriteString(i.gResultDataAddr())
-
-			builder.WriteString("# -------------- TEST BEGIN --------------\n")
-			builder.WriteString(i.gVsetvli(c.Vl, sew, c.LMUL))
-			builder.WriteString(fmt.Sprintf("%s v%d, (a0)%s\n", i.Name, vs3, v0t(c.Mask)))
-			builder.WriteString("# -------------- TEST END   --------------\n")
-
-			for nf := 0; nf < nfields; nf++ {
-				builder.WriteString(i.gLoadDataIntoRegisterGroup(vs3+(nf*int(lmul1)), lmul1, sew))
-				builder.WriteString(i.gMagicInsn(vs3+(nf*int(lmul1)), lmul1))
-			}
+		builder.WriteString(i.gWriteIntegerTestData(c.LMUL1*LMUL(nfields), c.SEW, 0))
+		for nf := 0; nf < nfields; nf++ {
+			builder.WriteString(i.gLoadDataIntoRegisterGroup(vs3+(nf*int(c.LMUL1)), c.LMUL1, c.SEW))
+			builder.WriteString(fmt.Sprintf("li a5, %d\n", i.vlenb()*int(c.LMUL1)))
+			builder.WriteString("add a0, a0, a5\n")
 		}
+
+		builder.WriteString(i.gResultDataAddr())
+
+		builder.WriteString("# -------------- TEST BEGIN --------------\n")
+		builder.WriteString(i.gVsetvli(c.Vl, c.SEW, c.LMUL))
+		builder.WriteString(fmt.Sprintf("%s v%d, (a0)%s\n", i.Name, vs3, v0t(c.Mask)))
+		builder.WriteString("# -------------- TEST END   --------------\n")
+
+		builder.WriteString(i.gLoadDataIntoRegisterGroup(vs3, c.LMUL1, c.SEW))
+		builder.WriteString(i.gMagicInsn(vs3, c.LMUL1))
+
 		res = append(res, builder.String())
 	}
 	return res
